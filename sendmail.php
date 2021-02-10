501c949001c2eeee056e9f00c2df90f8960c6bdc
ausian <51756660+ausian@users.noreply.github.com>
2021-02-10 22:56:10 +0300

    Add files via upload

---
+++ sendmail.php
@@ -0,0 +1,74 @@
+<?php
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+
+	require 'phpmailer/src/Exception.php';
+	require 'phpmailer/src/PHPMailer.php';
+
+	$mail = new PHPMailer(true);
+	$mail->CharSet = 'UTF-8';
+	$mail->setLanguage('ru', 'phpmailer/language/');
+	$mail->IsHTML(true);
+
+	//От кого письмо
+	$mail->setFrom('info@rguk-tilp.ru', 'Технологический институт легкой промышленности');
+	//Кому отправить
+	$mail->addAddress('tilp@rguk.ru');
+	//Тема письма
+	$mail->Subject = 'Новая Заявка';
+
+
+
+	//Тело письма
+	$body = '<h1>Заявка</h1>';
+	
+	if(trim(!empty($_POST['name']))){
+		$body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
+	}
+	if(trim(!empty($_POST['surname']))){
+		$body.='<p><strong>Фамилия:</strong> '.$_POST['surname'].'</p>';
+	}
+	if(trim(!empty($_POST['patronymic']))){
+		$body.='<p><strong>Отчество:</strong> '.$_POST['patronymic'].'</p>';
+	}
+
+	if(trim(!empty($_POST['email']))){
+		$body.='<p><strong>E-mail:</strong> '.$_POST['email'].'</p>';
+	}
+	if(trim(!empty($_POST['tel']))){
+		$body.='<p><strong>Телефон:</strong> '.$_POST['tel'].'</p>';
+	}
+	if(trim(!empty($_POST['name']))){
+		$body.='<p><strong>Регион:</strong> '.$_POST['region'].'</p>';
+	}
+	
+	if(trim(!empty($_POST['message']))){
+		$body.='<p><strong>Сообщение:</strong> '.$_POST['message'].'</p>';
+	}
+	
+	//Прикрепить файл
+	if (!empty($_FILES['image']['tmp_name'])) {
+		//путь загрузки файла
+		$filePath = __DIR__ . "/files/" . $_FILES['image']['name']; 
+		//грузим файл
+		if (copy($_FILES['image']['tmp_name'], $filePath)){
+			$fileAttach = $filePath;
+			$body.='<p><strong>Фото в приложении</strong>';
+			$mail->addAttachment($fileAttach);
+		}
+	}
+
+	$mail->Body = $body;
+
+	//Отправляем
+	if (!$mail->send()) {
+		$message = 'Ошибка';
+	} else {
+		$message = 'Данные отправлены!';
+	}
+
+	$response = ['message' => $message];
+
+	header('Content-type: application/json');
+	echo json_encode($response);
+?>
